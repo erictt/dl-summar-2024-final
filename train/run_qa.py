4b1b3b1f253dec46ed018d95d6d461ef992a374d
Eric Yang <ericyy87@gmail.com>
2024-07-03 23:41:54 -0400

    fix version

--- train/run_qa.py
+++ train/run_qa.py
@@ -50,11 +50,11 @@ from transformers.utils.versions import require_version
 from utils_qa import postprocess_qa_predictions
 
 # Will error if the minimal version of Transformers is not installed. Remove at your own risks.
-check_min_version("4.40.0")
+check_min_version("4.40.2")
 
 require_version(
     "datasets>=1.8.0",
-    "To fix: pip install -r examples/pytorch/question-answering/requirements.txt",
+    "To fix: pip install -r ./requirements.txt",
 )
 
 logger = logging.getLogger(__name__)
