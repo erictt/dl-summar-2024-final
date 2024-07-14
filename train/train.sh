52bf8d386cfbfa5c031b47df1e652a1cdc167a13
Eric Yang <ericyy87@gmail.com>
2024-07-13 22:42:01 -0400

    fix teh training script

---
+++ train/train.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+python3 run_qa.py \
+	--model_name_or_path roberta-base \
+	--dataset_name squad_v2 \
+	--do_train \
+	--do_eval \
+	--per_device_train_batch_size 16 \
+	--learning_rate 1e-4 \
+	--num_train_epochs 3 \
+	--max_seq_length 384 \
+	--doc_stride 128 \
+	--output_dir ./seq_bn/ \
+	--train_adapter \
+	--adapter_config ./configs/adapter_config_seq_bn_default.json \
+	--version_2_with_negative \
+	--overwrite_output_dir
+
+python3 run_qa.py \
+	--model_name_or_path roberta-base \
+	--dataset_name squad_v2 \
+	--do_train \
+	--do_eval \
+	--per_device_train_batch_size 16 \
+	--learning_rate 1e-4 \
+	--num_train_epochs 3 \
+	--max_seq_length 384 \
+	--doc_stride 128 \
+	--output_dir ./pf/ \
+	--train_adapter \
+	--adapter_config ./configs/adapter_config_pf_default.json \
+	--version_2_with_negative \
+	--overwrite_output_dir
